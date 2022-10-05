61e6c079623b867bdb60793066e60700bb9bf9c4
Makenna Gilbert <makennasgilbert@gmail.com>
2022-10-05 12:11:38 -0700

    Create a function to return a reversed string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(userInput) {
+    const chars = [];
+    for (i = 0; i < (userInput.length); i++) {
+        chars[i] = userInput.substring(i, (i + 1));
+    }
+    const reverseChars = chars.reverse();
+    //console.log(reverseChars);
+    let output = "";
+    for (i = 0; i < reverseChars.length; i++) {
+        output = output.concat(reverseChars[i]);
+    }
+    return output;
 };
 
 // Do not edit below this line
