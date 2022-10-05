5990ec462a0c7e440e6452324db9449f14357b59
Makenna Gilbert <makennasgilbert@gmail.com>
2022-10-05 11:23:30 -0700

    Create function to produce repeated string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function(phrase, num) {
+    let result = "";
+    if (num < 0) {
+        result = "ERROR"
+    }
+    for (let i = 0; i < num; i++) {
+        result = result + phrase
+    }
+    return result;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
