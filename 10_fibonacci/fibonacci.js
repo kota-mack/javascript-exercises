3b76c399f953b43b40e40fbe38185d257f73a696
Makenna Gilbert <makennasgilbert@gmail.com>
2022-10-07 11:28:57 -0700

    Create a function to return the nth Fibonacci number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    //if negative return "OOPS"
+    let n = 1;
+    if (input < 1) {
+        return "OOPS";
+    } else {
+        //use for loop to iterate fibonacci sequence up to and including specified number
+        for (i = 0; i < input; i++) {
+            n = (n - 1) + (n - 2);
+        }
+        //return nth number
+        return n;
+    }
 };
+
+console.log(fibonacci(4));
+console.log(fibonacci(25));
+
 
 // Do not edit below this line
 module.exports = fibonacci;
