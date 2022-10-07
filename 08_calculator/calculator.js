46c866ae71bbccf7716e7979fb011926abdd96d4
Makenna Gilbert <makennasgilbert@gmail.com>
2022-10-06 17:11:10 -0700

    Create functions for calculations

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,54 @@
-const add = function() {
-	
+const add = function(a, b) {
+  let output = a + b;
+  return output;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  let output = a - b;
+  return output;
 };
 
-const sum = function() {
-	
+const sum = function(items) {
+  const inputArray = Array.from(items);
+  let output = (0);
+  for (i = 0; i < inputArray.length; i++) {
+    output = ((output) + (inputArray[i]));
+}
+  return output;
 };
 
-const multiply = function() {
-
+const multiply = function(items) {
+  const inputArray = Array.from(items);
+  let output = 1;
+  for (i = 0; i < inputArray.length; i++) {
+    output = ((output) * (inputArray[i]));
+  }
+  return output;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let output = (a ** b);
+  return output;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) {
+    a = 1;
+  } else {
+    a = a;
+  }
+  let output = a;
+  const factors = [];
+  factors[0] = a;
+  for (i = 1; i < a; i++) {
+    factors[i] = (a - i);
+  }
+  for (i = 1; i < factors.length; i++) {
+    output = (output) * (factors[i]);
+  }
+  return output;
 };
+
 
 // Do not edit below this line
 module.exports = {
