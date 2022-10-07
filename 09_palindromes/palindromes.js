ea248c1f24af3dd32899a3d0e8c08fc1526fbec7
Makenna Gilbert <makennasgilbert@gmail.com>
2022-10-07 11:04:06 -0700

    Write function to determine palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,34 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    let str = input;
+    const punctuationString = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+    const punctuationArray = Array.from(punctuationString);
+    const palinArray = [];
+    str = str.toLowerCase();
+    for (s = 0; s < str.length; s++) {
+        for (i = 0; i < punctuationArray.length; i++) {
+            if (str[s] == punctuationArray[i]) {
+                str = str.replace(str[s], "");
+            } else {
+                continue;
+            }
+        }
 
+    }
+    let newString = str.replace(/ /g, "");
+    for (s = 0; s < newString.length; s++) {
+        palinArray[s] = newString[s];
+        }
+    let originalPhrase = palinArray.join("");
+    let reverseArray = palinArray.reverse();
+    let reversePhrase = reverseArray.join("");
+    //console.log("Original: ", originalPhrase);
+    //console.log("Reverse: ", reversePhrase);
+    if (reversePhrase === originalPhrase) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
