9e100473fbdc34cdff28ff2aba2886bc6b6b1d63
Makenna Gilbert <makennasgilbert@gmail.com>
2022-10-07 12:10:18 -0700

    Create function to return an array of keys from object

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(items) {
+    let bookObject = {};
+    for (i = 0; i < items.length; i++) {
+        bookObject[items[i].title] = items[i].author;
+    }
+    return Object.keys(bookObject);
 };
 
 // Do not edit below this line
