519ffda949fe70c3a22d55981e17c45d6652bcfd
Makenna Gilbert <makennasgilbert@gmail.com>
2022-10-08 11:56:10 -0700

    Write function to convert a string to unicode

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,25 @@
-const caesar = function() {
-
+const caesar = function(input, shift) {
+    const punctuationString = ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+    const punctuationArray = Array.from(punctuationString);
+    let toCode = [];
+    for (i = 0; i < input.length; i++) {
+        for (p = 0; p < punctuationArray.length; p++) {
+            if (input[i] == punctuationArray[p]) {
+                toCode[i] = input[i];
+            } else {
+                continue;
+            }
+        }
+        if (toCode[i] == input[i]) {
+            continue;
+        } else {
+            toCode[i] = input.charCodeAt(i);
+        }
+    }
+    return toCode;
 };
+
+console.log(caesar("a,b! c"));
 
 // Do not edit below this line
 module.exports = caesar;
